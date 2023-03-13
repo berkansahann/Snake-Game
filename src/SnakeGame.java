bbfef925fef0288f7b05e0817284f2f13e816921
Berkan <berkansahanofficial@gmail.com>
2023-03-13 03:01:05 +0300

    Initial commit

---
+++ src/SnakeGame.java
@@ -0,0 +1,17 @@
+public class SnakeGame {
+
+    public static void main(String[] args){
+
+        new GameFrame();
+    }
+}
+
+/*
+duvar seçeneği eklenecek
+menu yapılacak
+duvar görsel olarak da gelecek
+oyunu yeniden başlatma gelebilir
+high score tutulabilir
+high core listesi
+app fotosu
+ */
